8e6ec2be87afc92623cabaab4d03faf23e74a3e4
исп-31 <-31@LAB24-03>
2021-11-12 09:36:14 +0300

    Добавьте файлы проекта.

---
+++ pract9/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pract9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Runner runner = new Runner();
+
+        private void справка_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ИСП-31\nПеревезенцева Полина\n\nЗадание:\nЗаполнить таблицу участников забега на 100 метров на 8 человек с полями: ФИО, номер, результат. Вывести результат на экран. Вывести средний результат.");
+        }
+
+        private void выход_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string [,] matrix = new string [8,3];
+            matrix = runner.FillMatrix(matrix);
+            dataGrid.ItemsSource = VisualArray.ToDataTable(matrix).DefaultView;
+            dataGrid.Columns[0].Header = "Номер";
+            dataGrid.Columns[1].Header = "ФИО";
+            dataGrid.Columns[2].Header = "Результат";
+            result.Text = runner.AverageValue(matrix);
+        }
+    }
+}
---
+++ pract9/Runner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pract9
+{
+    public struct Runner
+    {
+        public string[,] FillMatrix(string[,] matrix)
+        {
+            matrix = new string[8, 3];
+            Random rnd = new Random();
+            for (int i = 0; i < 8; i++)
+            {
+                matrix[i, 2] = rnd.Next(1, 50).ToString();
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                matrix[i, 0] = (i + 1).ToString();
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                matrix[i, 1] = "Имя " + (i + 1);
+            }
+            return matrix;
+        }
+
+        public string AverageValue (string [,] matrix)
+        {
+
+            Random rnd = new Random();
+            int counter = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                counter += Convert.ToInt32(matrix[i, 2]);
+            }
+            counter /= 8;
+            string counterText = counter.ToString();
+            return counterText;
+        }
+    }
+}
